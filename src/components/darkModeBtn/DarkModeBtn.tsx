bb67566647f9eb04b6f61b2909c27f684232c7d4
haimin <623322116@qq.com>
2024-07-31 21:34:33 +0800

    feat:darkmode暂存

--- src/components/darkModeBtn/DarkModeBtn.tsx
+++ src/components/darkModeBtn/DarkModeBtn.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { removeLS, getLS, setLS } from '@/utils';
-import React, { useLayoutEffect } from 'react';
+import { SunOutlined } from '@ant-design/icons';
+import React, { useLayoutEffect, useState } from 'react';
 
 interface IProps {
     className?: string;
@@ -8,42 +9,12 @@ interface IProps {
 
 export default function DarkModeBtn(props: IProps) {
     const { className } = props;
-    const rootElement = document.documentElement; // <html>
+    const [rootElement, setRootElement] = useState<any>(); // <html>
     const darkModeStorageKey = 'user-color-scheme'; // 作为 localStorage 的 key
-    const getModeFromCSSMediaQuery = () => {
-        const res = rootElement.classList.contains('dark');
-        console.log(res);
-
-        return res ? 'dark' : 'light';
-    };
-
-    const resetRootDarkModeAttributeAndLS = () => {
-        rootElement.classList.remove('dark');
-        //removeLS(darkModeStorageKey);
-    };
 
     const validColorModeKeys: any = {
         dark: true,
         light: true,
-    };
-
-    const applyCustomDarkModeSettings = (mode?: any) => {
-        // 接受从「开关」处传来的模式，或者从 localStorage 读取
-        const currentSetting = mode || getLS(darkModeStorageKey);
-        if (currentSetting === getModeFromCSSMediaQuery()) {
-            // 当用户自定义的显示模式和 prefers-color-scheme 相同时重置、恢复到自动模式
-            resetRootDarkModeAttributeAndLS();
-        } else if (validColorModeKeys[currentSetting]) {
-            if (currentSetting === 'light') {
-                rootElement.classList.remove('dark');
-            } else {
-                rootElement.classList.add('dark');
-            }
-        } else {
-            // 首次访问或从未使用过开关、localStorage 中没有存储的值，currentSetting 是 null
-            // 或者 localStorage 被篡改，currentSetting 不是合法值
-            resetRootDarkModeAttributeAndLS();
-        }
     };
 
     const invertDarkModeObj: any = {
@@ -51,40 +22,57 @@ export default function DarkModeBtn(props: IProps) {
         light: 'dark',
     };
 
+    const getModeFromClassList = () => {
+        const res = rootElement?.classList.contains('dark');
+        console.log(res);
+
+        return res ? 'dark' : 'light';
+    };
+
     const toggleCustomDarkMode = () => {
         let currentSetting: any = getLS(darkModeStorageKey);
-
         if (validColorModeKeys[currentSetting]) {
-            // 从 localStorage 中读取模式，并取相反的模式
             currentSetting = invertDarkModeObj[currentSetting];
         } else if (currentSetting === null) {
-            // localStorage 中没有相关值，或者 localStorage 抛了 Error
-            // 从 CSS 中读取当前 prefers-color-scheme 并取相反的模式
-            currentSetting = invertDarkModeObj[getModeFromCSSMediaQuery()];
+            currentSetting = invertDarkModeObj[getModeFromClassList()];
         } else {
-            // 不知道出了什么幺蛾子，比如 localStorage 被篡改成非法值
-            return; // 直接 return;
+            return;
         }
-        // 将相反的模式写入 localStorage
         setLS(darkModeStorageKey, currentSetting);
 
         return currentSetting;
     };
 
-    // 当页面加载时，将显示模式设置为 localStorage 中自定义的值（如果有的话）
+    const applyCustomDarkModeSettings = (mode?: any) => {
+        const currentSetting = mode || getLS(darkModeStorageKey);
+        if (validColorModeKeys[currentSetting]) {
+            if (currentSetting === 'light') {
+                rootElement?.classList.remove('dark');
+            } else {
+                rootElement?.classList.add('dark');
+            }
+        } else {
+            rootElement?.classList.remove('dark');
+            removeLS(darkModeStorageKey);
+        }
+    };
+
     useLayoutEffect(() => {
-        //console.log('/////', window.matchMedia('(prefers-color-scheme: dark)').matches);
-        applyCustomDarkModeSettings();
+        setRootElement(document.documentElement);
     }, []);
+
+    useLayoutEffect(() => {
+        if (rootElement) applyCustomDarkModeSettings();
+    }, [rootElement]);
 
     return (
         <div
-            className={`flex h-36 ${className}`}
+            className={`flex ${className}`}
             onClick={() => {
                 applyCustomDarkModeSettings(toggleCustomDarkMode());
             }}
         >
-            123
+            <SunOutlined />
         </div>
     );
 }
