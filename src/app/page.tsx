f0f082dc79a85e71ebbbb63e6ed0223fe4ad6cb1
施海敏 <623322116@qq.com>
2024-08-12 11:15:30 +0800

    feat: 加载调整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from '@/components/footer/Footer';
 import PostList from '@/components/postList/PostList';
+import { Suspense } from 'react';
 export default function Home() {
     return (
-        <>
+        <Suspense>
             <div className="flex h-full flex-col items-center">
                 <PostList></PostList>
             </div>
             <Footer></Footer>
-        </>
+        </Suspense>
     );
 }
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,9 +1,12 @@
 import PostList from '@/components/postList/PostList';
+import { Suspense } from 'react';
 
 export default function Home() {
     return (
-        <div className="flex h-full flex-col items-center justify-between">
-            <PostList></PostList>
-        </div>
+        <Suspense>
+            <div className="flex h-full flex-col items-center justify-between">
+                <PostList></PostList>
+            </div>
+        </Suspense>
     );
 }
