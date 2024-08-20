e3cce34f74628ab496e8d752453cf3b5492e750b
施海敏 <623322116@qq.com>
2024-08-20 10:59:36 +0800

    feat: done code copy

--- src/components/mdx/copyButton.tsx
+++ src/components/mdx/copyButton.tsx
@@ -1,15 +1,15 @@
 'use client';
 import clsx from 'clsx';
-import React from 'react';
+import React, { ReactNode, useRef, useState } from 'react';
 
 interface ICopyToClipboard {
-	children: React.ReactChild;
+	children: ReactNode;
 }
 
 export const CopyButton = ({ children }: ICopyToClipboard) => {
-	const textInput = React.useRef<HTMLDivElement>(null);
-	const [hovered, setHovered] = React.useState(false);
-	const [copied, setCopied] = React.useState(false);
+	const textInput = useRef<HTMLDivElement>(null);
+	const [hovered, setHovered] = useState(false);
+	const [copied, setCopied] = useState(false);
 
 	const onEnter = () => {
 		setHovered(true);
@@ -35,37 +35,35 @@ export const CopyButton = ({ children }: ICopyToClipboard) => {
 				<button
 					aria-label="Copy code"
 					type="button"
-					className={clsx('absolute right-2 top-2 h-8 w-8 rounded border-2 bg-gray-700 p-1 dark:bg-gray-800', {
-						'border-green-400 focus:border-green-400 focus:outline-none': copied,
-						'hover:border-gray-300': !copied,
-					})}
+					className={clsx(
+						'absolute right-2 top-2 h-8 w-8 rounded border-2 bg-gray-700 p-1 text-font-light-dark dark:bg-gray-800',
+						{
+							'border-green-400': copied,
+							'hover:border-gray-300': !copied,
+						}
+					)}
 					onClick={onCopy}
 				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
-						stroke="currentColor"
+						stroke={copied ? '#00ff00' : '#ffffff'}
 						fill="none"
-						className={copied ? 'text-green-400' : 'text-gray-300'}
 					>
 						{copied ? (
-							<>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-								/>
-							</>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+							/>
 						) : (
-							<>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-								/>
-							</>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+							/>
 						)}
 					</svg>
 				</button>
