c9a97c89eb48ea810b13681949b57acebee145ec
施海敏 <623322116@qq.com>
2024-08-20 17:38:57 +0800

    feat:edit sitmap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,19 +15,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 0.5,
 		},
 		{
-			url: 'http://roadto.top/dashboard',
+			url: 'http://roadto.top/friendlyLink/',
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5,
+		},
+		{
+			url: 'http://roadto.top/post/',
 			lastModified: new Date(),
 			changeFrequency: 'daily',
 			priority: 0.8,
 		},
 		{
-			url: 'http://roadto.top/friendLink',
+			url: 'http://roadto.top/post/create-my-blog/',
 			lastModified: new Date(),
 			changeFrequency: 'daily',
 			priority: 0.8,
 		},
 		{
-			url: 'http://roadto.top/post',
+			url: 'http://roadto.top/post/components-of-my-blog/',
 			lastModified: new Date(),
 			changeFrequency: 'daily',
 			priority: 0.8,
