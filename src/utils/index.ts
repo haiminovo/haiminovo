7156604a7251a42b95a0b80440326a922cb0ec3b
施海敏 <623322116@qq.com>
2025-02-06 10:29:57 +0800

    feat: getRandomColor changed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,10 +22,7 @@ export function getRandom(min: number, max: number) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-export function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-	const b = Math.floor(Math.random() * 256);
-	const a = Math.random();
-    return `rgba(${r},${g},${b},${a})`;
+export function getRandomColor(): string {
+    const randomRGB = () => Math.floor(Math.random() * 256);
+    return `rgba(${randomRGB()},${randomRGB()},${randomRGB()},${Math.random().toFixed(2)})`;
 }
