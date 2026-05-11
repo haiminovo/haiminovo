357794e343c49f2361a9d82b958582c0a502b5ad
haimin <623322116@qq.com>
2026-05-11 16:23:17 +0800

    feat: 优化

--- src/components/digitalClock/digitalNumber/baseStick/BaseStick.tsx
+++ src/components/digitalClock/digitalNumber/baseStick/BaseStick.tsx
@@ -12,21 +12,36 @@ const directionDegreeMap = {
 };
 export default function BaseStick(props: IProps) {
 	const { active = true, direction = 'bottom' } = props;
-	if (!active) return <div className="h-3 w-12"></div>;
-	const basicStickStyle =
-		'relative bg-custom-color-dark-4 dark:bg-custom-color-4 h-3 w-12 transition-all duration-700 ' +
-		`before:absolute before:left-0 before:w-3 before:h-3 before:bg-custom-color-2 dark:before:bg-custom-color-dark-2 ` +
-		`after:absolute after:right-0 after:w-3 after:h-3 after:bg-custom-color-2 dark:after:bg-custom-color-dark-2 `;
-	const directionStickStyle =
-		'before:[clip-path:polygon(_0%_0%,_-100%_100%,_100%_0%,_0%_0%)] ' +
-		'after:[clip-path:polygon(_0%_0%,_200%_100%,_100%_0%,_0%_0%)] ' +
-		`${directionDegreeMap[direction]}`;
-	const midStickStyle =
-		'before:[clip-path:polygon(_0%_0%,_100%_0%,_0%_25%,_0%_75%,_100%_100%,_0%_100%,_0%_0%)] ' +
-		'after:[clip-path:polygon(_0%_0%,_100%_0%,_100%_100%,_0%_100%,_100%_75%,_100%_25%,_0%_0%)] ';
-	return direction === 'mid' ? (
-		<div className={`${basicStickStyle + midStickStyle} `}></div>
-	) : (
-		<div className={`${basicStickStyle + directionStickStyle} `}></div>
+	if (!active) return <div className="h-2 w-12"></div>;
+	const svgClass = `h-2 w-12 transition-all duration-700 ${directionDegreeMap[direction]}`;
+
+	if (direction === 'mid') {
+		return (
+			<svg
+				viewBox="0 0 48 8"
+				aria-hidden="true"
+				className={svgClass}
+				xmlns="http://www.w3.org/2000/svg"
+				preserveAspectRatio="none"
+			>
+				<path className="fill-custom-color-dark-4 dark:fill-custom-color-4" d="M6 0H42L48 4L42 8H6L0 4L6 0Z" />
+				<path className="fill-custom-color-2 dark:fill-custom-color-dark-2" d="M0 4L6 0V2L3 4L6 6V8L0 4Z" />
+				<path className="fill-custom-color-2 dark:fill-custom-color-dark-2" d="M48 4L42 0V2L45 4L42 6V8L48 4Z" />
+			</svg>
+		);
+	}
+
+	return (
+		<svg
+			viewBox="0 0 48 8"
+			aria-hidden="true"
+			className={svgClass}
+			xmlns="http://www.w3.org/2000/svg"
+			preserveAspectRatio="none"
+		>
+			<path className="fill-custom-color-dark-4 dark:fill-custom-color-4" d="M4 0H44L40 8H8L4 0Z" />
+			<path className="fill-custom-color-2 dark:fill-custom-color-dark-2" d="M0 0H4L8 8H4L0 0Z" />
+			<path className="fill-custom-color-2 dark:fill-custom-color-dark-2" d="M44 0H48L44 8H40L44 0Z" />
+		</svg>
 	);
 }
